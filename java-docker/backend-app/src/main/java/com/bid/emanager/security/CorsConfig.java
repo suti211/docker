bd53ec54c545581277eaeb59b0e6220c6ad211cc
suti211 <bosnyak.imre@otpebiz.hu>
2019-02-19 16:26:54 +0100

    BE - CORS dev config

---
+++ java-docker/backend-app/src/main/java/com/bid/emanager/security/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.bid.emanager.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Profile("dev")
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost")
+                .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "HEAD")
+                .allowCredentials(true);
+    }
+
+}
