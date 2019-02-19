c067638ad84600e35cf2975f19f861e24be6cedc
suti211 <bosnyak.imre@otpebiz.hu>
2019-02-19 16:29:02 +0100

    BE - unused import removal

--- java-docker/backend-app/src/main/java/com/bid/emanager/EManagerApplication.java
+++ java-docker/backend-app/src/main/java/com/bid/emanager/EManagerApplication.java
@@ -1,6 +1,5 @@
 package com.bid.emanager;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
