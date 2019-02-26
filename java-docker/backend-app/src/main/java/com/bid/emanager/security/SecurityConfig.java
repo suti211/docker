6be6d58d880afe9721100bfa186ced9ad5570910
suti211 <bosnyak.imre@otpebiz.hu>
2019-02-26 16:31:29 +0100

    BE - incoming request logging

--- java-docker/backend-app/src/main/java/com/bid/emanager/security/SecurityConfig.java
+++ java-docker/backend-app/src/main/java/com/bid/emanager/security/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.bid.emanager.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 import com.bid.emanager.authentication.RestAuthenticationEntryPoint;
 import com.bid.emanager.authentication.SuccessfulAuthenticationHandler;
@@ -45,6 +47,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+	}
+	
+	@Bean
+	@Profile("dev")
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+	    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+	    loggingFilter.setIncludeClientInfo(true);
+	    loggingFilter.setIncludeQueryString(true);
+	    loggingFilter.setIncludePayload(true);
+	    return loggingFilter;
 	}
 
 }
