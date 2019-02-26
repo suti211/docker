813f18c69e7b0055d2dffb55b26b973efd149fbf
suti211 <bosnyak.imre@otpebiz.hu>
2019-02-26 16:32:26 +0100

    BE - DEV CORS settings

--- java-docker/backend-app/src/main/java/com/bid/emanager/security/CorsConfig.java
+++ java-docker/backend-app/src/main/java/com/bid/emanager/security/CorsConfig.java
@@ -1,20 +1,27 @@
 package com.bid.emanager.security;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Profile("dev")
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost")
-                .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "HEAD")
-                .allowCredentials(true);
-    }
+public class CorsConfig {
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("*"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 
 }
