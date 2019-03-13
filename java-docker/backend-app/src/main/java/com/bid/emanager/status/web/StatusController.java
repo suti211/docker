9475f4357abd2ffa99072a90a8914b522dfd4fc6
suti211 <bosnyak.imre@otpebiz.hu>
2019-03-13 16:20:57 +0100

    BE - cleaning imports

--- java-docker/backend-app/src/main/java/com/bid/emanager/status/web/StatusController.java
+++ java-docker/backend-app/src/main/java/com/bid/emanager/status/web/StatusController.java
@@ -1,12 +1,6 @@
 package com.bid.emanager.status.web;
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
