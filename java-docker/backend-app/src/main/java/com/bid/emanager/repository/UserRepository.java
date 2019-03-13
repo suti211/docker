ab328aa24c794b93dcce64a9cabf013699d78e1b
suti211 <bosnyak.imre@otpebiz.hu>
2019-03-13 16:20:28 +0100

    BE - using HQL

--- java-docker/backend-app/src/main/java/com/bid/emanager/repository/UserRepository.java
+++ java-docker/backend-app/src/main/java/com/bid/emanager/repository/UserRepository.java
@@ -2,12 +2,15 @@ package com.bid.emanager.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.bid.emanager.entity.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-	Optional<User> findByEmail(String email);
+	@Query("SELECT u FROM User u where u.email = :email")
+	Optional<User> findByEmail(@Param("email") String email);
 }
